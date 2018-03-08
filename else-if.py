8a392d037b8533cab7dd5fe0ff5f9f7e3654be47
Joseph Githumbi <githumbi74@gmail.com>
2018-03-08 17:44:51 +0300

    Create else-if.py
    this is an example of else if statement

---
+++ else-if.py
@@ -0,0 +1,12 @@
+"""
+an if else statement
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+    if (n%2 == 1) or (n%2 == 0 and n in range (6,21)) :
+        print ("Weird")
+    else:
+                print("Not Weird")
+            
