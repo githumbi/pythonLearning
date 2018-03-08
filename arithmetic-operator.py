46d1d75c852c234d0fe1e8e8d90f7e73b43e010a
Joseph Githumbi <githumbi74@gmail.com>
2018-03-08 17:53:57 +0300

    Create arithmetic-operator.py
    Task
    Read two integers from STDIN and print three lines where:
    
        The first line contains the sum of the two numbers.
        The second line contains the difference of the two numbers (first - second).
        The third line contains the product of the two numbers.

---
+++ arithmetic-operator.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+if a in range (1,100) and b in range (1,100):  
+        print (a+b)
+        print (a-b)
+        print (a*b)
